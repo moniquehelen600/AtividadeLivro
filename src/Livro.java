70b8782de9efb0b93465b73f0a2ca4170d05ec1c
monique <moniquehelen600@gmail.com>
2023-10-17 19:44:49 -0300

    Atividade Livro

---
+++ src/Livro.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.CDATASection;
+import java.time.LocalDate;
+
+public class ListaLivro {
+    public String titulo;
+    public Autor autor;
+    public float preco;
+    public LocalDate datalancamento;
+
+    public String nome;
+
+}
